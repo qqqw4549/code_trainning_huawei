e4515c25289cbecd4f99a20949968509e1c5ef24
WangLiDong <2388684549@qq.com>
2020-02-10 12:53:46 +0800

    2020/02/10

--- test.c
+++ test.c
@@ -2,41 +2,27 @@
 #include<string.h>
 #include <malloc.h>
 
-int repeat_check(char *str)
-{
-    char temp[4];
-    temp[3] = '\0';
-    for(;*(str+2) != '\0'; str++)
-    {
-        memcpy(temp, str, 3);
-        if (strstr(str+3, temp)!=NULL)
-        {
-            return 0;
+char * get_pwd(char * str){
+    int i=0,len=strlen(str);
+    for(i=0;i<len;i++){
+        if('A'<=str[i] && str[i]<'Z')str[i]=str[i]+33;
+        else if('Z'==str[i])str[i]='a';
+        else if('a'<=str[i] && str[i]<='o'){
+            str[i]=(str[i]-'a')/3+2+'0';
         }
-    }
-    return 1;
+        else if('o'<str[i] && str[i]<'t')str[i]='0'+7;
+        else if('s'<str[i] && str[i]<'w')str[i]='0'+8;
+        else if('v'<str[i] && str[i]<='z')str[i]='0'+9;
+    }  
+    return str;
 }
+
 int main()
 {
     char pwd[100];
-
     int i=0,count=0;
     while(scanf("%s",pwd)!=EOF){
-        int N=0,H=0,L=0,S=0,F=0;
-        F=repeat_check(pwd);
-        for(i=0;i<strlen(pwd);i++){
-        if( 'a' <=pwd[i] && pwd[i]<= 'z')L=1;
-            if( 'A' <=pwd[i] && pwd[i]<= 'Z')H=1;
-            if( '0' <=pwd[i] && pwd[i]<= '9')N=1;
-            else S=1;
-        }
-
-        //printf("%d",N+H+L+S);
-        if((N+H+L+S)<3 || F==0 || strlen(pwd)<=8 )
-        printf("%s\n","NG");
-        else printf("%s\n","OK");
-        count++;
+        printf("%s\n",get_pwd(pwd));
     } 
-
     return 0;
 }   
--- 刷题.c
+++ 刷题.c
@@ -628,9 +628,52 @@ int main()
 3.不能有相同长度超2的子串重复
 说明:长度超过2的子串
 */
+#include<stdio.h>
+#include<string.h>
+#include <malloc.h>
 
+int repeat_check(char *str)
+{
+    char temp[4];
+    temp[3] = '\0';
+    for(;*(str+2) != '\0'; str++)
+    {
+        memcpy(temp, str, 3);
+        if (strstr(str+3, temp)!=NULL)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    char pwd[100];
 
+    int i=0,count=0;
+    while(scanf("%s",pwd)!=EOF){
+        int N=0,H=0,L=0,S=0,F=0;
+        F=repeat_check(pwd);
+        for(i=0;i<strlen(pwd);i++){
+        if( 'a' <=pwd[i] && pwd[i]<= 'z')L=1;
+            if( 'A' <=pwd[i] && pwd[i]<= 'Z')H=1;
+            if( '0' <=pwd[i] && pwd[i]<= '9')N=1;
+            else S=1;
+        }
 
+        //printf("%d",N+H+L+S);
+        if((N+H+L+S)<3 || F==0 || strlen(pwd)<=8 )
+        printf("%s\n","NG");
+        else printf("%s\n","OK");
+        count++;
+    } 
+
+    return 0;
+}   
+
+/*
+
+*/
 
 
 
